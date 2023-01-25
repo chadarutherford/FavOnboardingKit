8fe8f180f86f349ccf938a9de0e8f149aa545f8e
chadarutherford <chad_rutherford@outlook.com>
2023-01-25 16:19:23 -0500

    Image extension?

---
+++ Sources/FavOnboardingKit/Utilities/Extensions/UIImage+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Chad Rutherford on 1/25/23.
+//
+
+import UIKit
+
+public extension UIImage {
+    static let slide1 = UIImage(named: "imSlide1") ?? UIImage()
+    static let slide2 = UIImage(named: "imSlide2") ?? UIImage()
+    static let slide3 = UIImage(named: "imSlide3") ?? UIImage()
+    static let slide4 = UIImage(named: "imSlide4") ?? UIImage()
+    static let slide5 = UIImage(named: "imSlide5") ?? UIImage()
+}
